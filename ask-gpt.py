ba30eade4990f0b8f5313bbd47ac8672b28a427c
wild0ni0n <yuisuwako@gmail.com>
2023-08-13 22:51:19 +0900

    add ask gpt program

--- ask-gpt.py
+++ ask-gpt.py
@@ -5,11 +5,17 @@ import key
 key.setenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-completion =openai.ChatCompletion.create(
-    model = "gpt-3.5-turbo",
-    messages=[
-        {"role": "user", "content": "hello"}
-    ]
-)
+def chatMsg(prompt):
+    completion =openai.ChatCompletion.create(
+        model = "gpt-3.5-turbo",
+        messages=[
+            {"role": "user", "content": prompt}
+        ]
+    )
 
-print(completion.choices[0].message)
+    return completion.choices[0].message["content"]
+
+if __name__ == "__main__":
+    prompt = "2023年4月現在の首相の名前は何でしょうか"
+    result = chatMsg(prompt)
+    print(result)
